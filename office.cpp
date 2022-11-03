b0060f7168da21d6d74d45c70a00f6da45be94dd
SHIVAM SHELKE <103065763+shvmshlk7@users.noreply.github.com>
2022-11-03 10:41:57 -0700

    Create office.cpp

---
+++ office.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,a,b;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>a>>b;
+	    int x=4*a+b;
+	    cout<<x<<endl;
+	}
+	return 0;
+}
